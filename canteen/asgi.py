de9db80bd87d233c206b8c87ef83e0f974eccf19
HarshavardhanVemali <vemalivardhan@gmail.com>
2024-11-07 17:12:33 +0530

    changescommit

--- canteen/asgi.py
+++ canteen/asgi.py
@@ -1,30 +1,22 @@
-"""
-ASGI config for canteen project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from canteenapp import routing
-
-
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path, re_path
+from django.urls import include
+import  canteenapp.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'canteen.settings')
-
-application = get_asgi_application()
-
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                canteenapp.routing.websocket_urlpatterns
+            )
         )
     ),
 })
