2b7c15f01c05bf7e2c126c98210306d7e7d4b40c
HarshavardhanVemali <vemalivardhan@gmail.com>
2024-11-07 17:58:58 +0530

    changescommit

--- canteen/asgi.py
+++ canteen/asgi.py
@@ -4,12 +4,15 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-import canteenapp.routing  # Import after django.setup() to ensure apps are ready
 
+# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'canteen.settings')
 
-# Set up Django
+# Initialize Django
 django.setup()
+
+# Import routing after setup to avoid AppRegistryNotReady error
+import canteenapp.routing
 
 # Define the ASGI application
 django_asgi_app = get_asgi_application()
