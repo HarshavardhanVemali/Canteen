796dcba45d9878b7f9b02a8b8906f0c50f977b06
HarshavardhanVemali <vemalivardhan@gmail.com>
2024-11-07 17:54:53 +0530

    changescommit

--- canteen/asgi.py
+++ canteen/asgi.py
@@ -4,16 +4,18 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+import canteenapp.routing  # Import after django.setup() to ensure apps are ready
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'canteen.settings')
 
 # Set up Django
 django.setup()
 
-import canteenapp.routing  # Import after django.setup() to ensure apps are ready
+# Define the ASGI application
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
